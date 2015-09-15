91250fd2dd77b7f1d7049710f348de0a970fa066
kkosta-lhd <kkosta@lefthanddesign.com>
2015-09-14 21:24:34 -0600

    gem array 9-14-15
    created gem information array

---
+++ functions.php
@@ -0,0 +1,8 @@
+<?php
+$n[] = $gem['mohs'];
+min($n);
+max($n);
+
+foreach ($gems as $gem => $n);
+
+?>
---
+++ gem-index.php
@@ -0,0 +1,83 @@
+<?php
+//require ('functions.php');
+
+$gems =
+    array (
+        array(
+            "name" => "diamond",
+            "structure" => 'octahedral',
+            "mohs" => '9',
+            "color" => 'white',
+            "image" => "https://en.wikipedia.org/wiki/Diamond#/media/File:Diamond.jpg"
+        ),
+        array (
+            "name" => "ruby",
+            "structure" => 'hexagonal',
+            "mohs" => '10',
+            "color" => 'red',
+            "image" => "https://en.wikipedia.org/wiki/Ruby#/media/File:Ruby_gem.JPG"
+
+        ),
+        array (
+            "name" => "emerald",
+            "structure" => 'octahedral',
+            "mohs" => '8',
+            "color" => 'green',
+            "image" => "https://en.wikipedia.org/wiki/Emerald#/media/File:Beryl_emeralds_cut_XH.jpg"
+
+        ),
+        array (
+            "name" => "saphhire",
+            "structure" => 'trigonal',
+            "mohs" => '9',
+            "color" => 'blue',
+            "image" => "https://en.wikipedia.org/wiki/Sapphire#/media/File:Yogo2783_Close_crop.JPG"
+
+        ),
+    )
+
+?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Using a foreach key value Loop</title>
+    <style>
+        #gems {margin:0px;padding:0px;border: solid 1px #666;}
+        #gems td {padding:10px;border: solid 1px #666;}
+        #gems tr.bold {font-weight:bold;background-color: #72fffa;}
+    </style>
+</head>
+
+<body>
+
+<h1>Gemstones</h1>
+<table id="gems" cellspacing="0">
+    <tr class="bold">
+        <td>Name</td>
+        <td>Color</td>
+        <td>Moh's</td>
+        <td>Structure</td>
+        <td>Image</td>
+
+    </tr>
+    <?php foreach($gems as $gem) {
+        $n[]=$gem['mohs'];
+        //the above statement creates an array by adding the moh's numbers each type it loops through the array
+        ?>
+    <tr>
+        <td><?php print $gem ["name"]; ?></td>
+        <td><?php print $gem ["color"]; ?></td>
+        <td><?php print $gem ["mohs"]; ?></td>
+        <td><?php print $gem ["structure"]; ?></td>
+        <td><a href ="<?php print $gem ["image"]; ?>">Photo of a <?php print $gem ["name"]; ?></a></td>
+
+    </tr>
+   <?php } ?>
+</table>
+<p>The hardness of the gems ranges from <?php print min($n); ?> to <?php print max($n); ?>.</p>
+
+
+</body>
+</html>
